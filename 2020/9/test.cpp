a5708be9bb3cc9ce0b960f9e8168f627b6fb5701
lympassion <455283303@qq.com>
2021-02-03 21:35:01 +0800

    整理 202012题目

---
+++ 2020/9/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+
+
+using namespace std;
+
+int main(){
+    double a = pow(0.5, 2);
+    if(5){
+        printf("%f", max(a, 0.3));
+    }
+    
+    return 0;
+}
