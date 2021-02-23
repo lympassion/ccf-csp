7d57093bbafdebb396237632461494a85dec779e
lympassion <455283303@qq.com>
2021-02-23 22:07:50 +0800

    12-2

--- test.cpp
+++ test.cpp
@@ -1,22 +1,61 @@
-#include <iostream>
-#include<algorithm>
+#include <stdio.h>
 
-using namespace std;
-
-int main()
-{
-	int a[15][10];
-	int b[4][4];
-	for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 4; j++){
-            b[i][j] = i+j;
+int main() {
+    int board[31][31] = {0};
+    bool remove[31][31] = {0};
+    int N, M, tst;
+    scanf("%d%d", &N, &M);
+    for(int n=0; n<N; n++) {
+        for(int m=0; m<M; m++) {
+            scanf("%d", &board[n][m]);
         }
     }
-
-    for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 4; j++){
-            b[i][j] = i ? i : ;
+    int color;
+    int cnt;
+    for(int n=0; n<N; n++) {
+        color = board[n][0];
+        cnt = 1;
+        for(int m=1; m<=M; m++) {
+            if(board[n][m]==color) {
+                cnt++;
+            }
+            else {
+                if(cnt>=3) {
+                    while(cnt) {
+                        tst = m-cnt;
+                        remove[n][m-cnt] = true;
+                        cnt--;
+                    }
+                }
+                color = board[n][m];
+                cnt = 1;
+            }
         }
     }
-	return 0;
+    for(int m=0; m<M; m++) {
+        color = board[0][m];
+        cnt = 1;
+        for(int n=1; n<=N; n++) {
+            if(board[n][m]==color) {
+                cnt++;
+            }
+            else {
+                if(cnt>=3) {
+                    while(cnt) {
+                        remove[n-cnt][m] = true;
+                        cnt--;
+                    }
+                }
+                color = board[n][m];
+                cnt = 1;
+            }
+        }
+    }
+    for(int n=0; n<N; n++) {
+        for(int m=0; m<M; m++) {
+            if(remove[n][m]) printf("%d ", 0);
+            else printf("%d ", board[n][m]);
+        }
+        printf("\n");
+    }
 }
